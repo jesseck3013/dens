f7f57c59eda657b8c2f0e3125863b28cc7fb8c31
jesseck3013 <jesseck3013@outlook.com>
2024-12-27 01:35:03 +0800

    tried sending udp diagram

--- dens.ConsoleApp/Program.cs
+++ dens.ConsoleApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+class Program
+{
+    static async Task Main()
+    {
+	UdpClient udpClient = new UdpClient();
+	
+	//	Byte[] data = Encoding.Unicode.GetBytes("hello");
+
+	var endpoint = IPEndPoint.Parse("1.1.1.1:53");
+
+	byte[] data = { 0x32, 0xD8, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x67, 0x6F, 0x6F, 0x67, 0x6C, 0x65, 0x03, 0x63, 0x6F, 0x6D, 0x00, 0x00, 0x1C, 0x00, 0x01 };
+	
+	await udpClient.SendAsync(data, data.Length, endpoint);
+	var response = await udpClient.ReceiveAsync();
+	foreach (var b in response.Buffer)
+	{
+	    Console.Write(b.ToString("X2") + " ");
+	}
+	Console.Write("\n");
+    }
+}
