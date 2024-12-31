42abd3d7d36a9932c88304f3b5d9dd5fc1e2b8d9
jesseck3013 <jesseck3013@outlook.com>
2024-12-31 15:46:10 +0800

    added test for header encode

---
+++ dens.Tests/MessageTest.cs
@@ -0,0 +1,37 @@
+﻿namespace dens.Tests;
+
+using dens.Core;
+
+public class HeaderTest
+{
+    [Fact]
+    public void EncodeTest1()
+    {
+	var header = new Header
+        {
+            ID = 0xAAAA,
+            QR = MessageType.Query,
+            OPCODE = QueryType.Query,
+            AA = false,
+            TC = false,
+            RD = true,
+            RA = false,
+            RCODE = ResponseType.IsQuery,
+            QDCOUNT = 0x01,
+            ANCOUNT = 0x00,
+            NSCOUNT = 0x00,
+            ARCOUNT = 0x00
+        };
+
+        var byteArray = header.Encode();
+
+	Byte[] expectedArray = new Byte[12] { 0xAA, 0xAA, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	Assert.Equal(expectedArray, byteArray);
+    }
+
+    [Fact]
+    public void DecodeTest1()
+    {
+
+    }
+}
