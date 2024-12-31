27295c08c264dd67bf09a408a56985871ef3f2ca
jesseck3013 <jesseck3013@outlook.com>
2024-12-31 15:56:58 +0800

    added test for header decode

--- dens.Tests/MessageTest.cs
+++ dens.Tests/MessageTest.cs
@@ -32,6 +32,20 @@ public class HeaderTest
     [Fact]
     public void DecodeTest1()
     {
+	Byte[] headerByte = new Byte[12] { 0xAA, 0xAA, 0x81, 0x80, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00 };
+	
+	var actual = Header.Decode(headerByte);
 
+	Assert.Equal(0xAAAA, actual.ID);
+	Assert.Equal(MessageType.Response, actual.QR);
+	Assert.Equal(QueryType.Query, actual.OPCODE);
+	Assert.False(actual.AA);
+	Assert.False(actual.TC);
+	Assert.True(actual.RD);
+	Assert.True(actual.RA);
+	Assert.Equal(1, actual.QDCOUNT);
+	Assert.Equal(1, actual.ANCOUNT);
+	Assert.Equal(0, actual.NSCOUNT);
+	Assert.Equal(0, actual.ARCOUNT);
     }
 }
